bb26a748a7085030b11a73acc9e75cf05b7ca0d3
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-07 18:49:09 -0400

    Update homepage_streamlit_app.py

--- homepage_streamlit_app.py
+++ homepage_streamlit_app.py
@@ -8,19 +8,19 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "HW Manager",
-        ["First Lab", "Second Lab"],
+        ["First Homework", "Second Homework"],
         icons=['book', 'beaker'],
         menu_icon="cast", 
         default_index=0,
     )
 
 # Load the appropriate page based on the user's selection
-if selected_page == "First Lab":
+if selected_page == "First Homework":
     st.title("HW - 1 ")
     # Execute the hw1.py code
     exec(open("hw1.py").read())  # This will run the content of hw1.py
 
-elif selected_page == "Second Lab":
+elif selected_page == "Second Homework":
     st.title("HW - 2")
     # Execute the hw2.py code
     exec(open("hw2.py").read())  # This will run the content of hw2.py
