807d4ee1a4977b6bba832cecf73023ac9c43463a
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-09 15:32:29 -0400

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,5 +1,8 @@
+import streamlit as st
+from openai import OpenAI
+
 # Show title and description.
-st.title("Karan Shah📄 Document Question Answering")
+st.title("HW-02-Karan Shah📄 Document Question Answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys)."
