f3e0d3b32ddac2be0ce689f63fae58beb310af99
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-18 12:04:31 -0400

    Update homepage_streamlit_app.py

--- homepage_streamlit_app.py
+++ homepage_streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "HW Manager",
-        ["First Homework", "Second Homework", "Third Homework"],
+        ["First Homework", "Second Homework", "Third Homework", "Test Gemini"],
         icons=['book', 'beaker'],
         menu_icon="cast", 
         default_index=0,
