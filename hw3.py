5cb471d93665a1457b5588e4f3d39d426d8a9762
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-18 11:52:53 -0400

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("HW-03-Karan Shah📄 Document question answering and Chatbot")
+st.title("HW-03-Karan Shah📄 Document question answering and Chatbot" OPEN AI )
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "You can also interact with the chatbot. "
