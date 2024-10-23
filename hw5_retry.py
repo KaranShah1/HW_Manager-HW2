a9646bcd7b3416f321c7380c69fa57b077d1c981
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-10-23 17:47:29 -0400

    Update hw5_retry.py

--- hw5_retry.py
+++ hw5_retry.py
@@ -296,5 +296,5 @@ def main():
             "Failed to create or load the document collection. Please check the zip file and try again.")
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
