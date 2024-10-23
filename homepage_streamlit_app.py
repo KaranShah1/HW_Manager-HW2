c7b3f246fba4b1924de33138e3247fa00c8e4a5a
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-10-23 17:56:54 -0400

    Update homepage_streamlit_app.py

--- homepage_streamlit_app.py
+++ homepage_streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "HW Manager",
-        ["First Homework", "Second Homework", "Third Homework", "Third Homework Retry", "Fourth Homework", "Fourth Homework Retry", "Fifth Homework","Fifth Homework Retry", "Sixth Homework"],
+        ["First Homework", "Second Homework", "Third Homework", "Third Homework Retry", "Fourth Homework", "Fourth Homework Retry", "Fifth Homework","trialll","Fifth Homework Retry", "Sixth Homework"],
         icons=['book', 'book', 'book', 'book', 'book','book','book','book','book'],
         menu_icon="cast", 
         default_index=0,
@@ -49,6 +49,11 @@ elif selected_page == "Fifth Homework":
     st.title("HW - 5")
     # Execute the hw5.py code
     exec(open("hw5.py").read())
+
+elif selected_page == "trialll":
+    st.title("HW - 5")
+    # Execute the hw5.py code
+    exec(open("HW5 (1).py.py").read())
 
 elif selected_page == "Fifth Homework Retry":
     st.title("HW - 5")
