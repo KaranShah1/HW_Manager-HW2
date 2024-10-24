5fedfdc80a825fb7fa906fd721e09fd715ea8559
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-10-24 18:25:33 -0400

    Update homepage_streamlit_app.py

--- homepage_streamlit_app.py
+++ homepage_streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "HW Manager",
-        ["First Homework", "Second Homework", "Third Homework", "Third Homework Retry", "Fourth Homework", "Fourth Homework Retry", "Fifth Homework","trialll","Fifth Homework Retry", "Sixth Homework"],
+        ["First Homework", "Second Homework", "Third Homework", "Third Homework Retry", "Fourth Homework", "Fourth Homework Retry", "Fifth Homework","Fifth Homework Retry", "Sixth Homework"],
         icons=['book', 'book', 'book', 'book', 'book','book','book','book','book'],
         menu_icon="cast", 
         default_index=0,
